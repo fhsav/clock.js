ab624db56ce07f99694220089a5192b73e725459
gluxon <bcheng.gt@gmail.com>
2013-09-01 19:24:15 -0400

    Start of something amazing

--- clock.js
+++ clock.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
+var stylus = require('stylus');
+
+var pub = __dirname + '/public';
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.locals.pretty = true;
+
+app.use(stylus.middleware(__dirname + '/public'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.render('clock');
 });
 
 app.listen(3000);
