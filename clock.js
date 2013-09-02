429d67a90d0611c6105d198a0581920d286b55c9
gluxon <bcheng.gt@gmail.com>
2013-09-02 02:12:06 -0400

    Basic login page

--- clock.js
+++ clock.js
@@ -1,22 +1,51 @@
 var express = require('express');
 var app = express();
 var stylus = require('stylus');
+var format = require('util').format;
+var uuid = require('node-uuid');
+require('js-yaml');
 
-var pub = __dirname + '/public';
+var MongoClient = require('mongodb').MongoClient;
+var Grid = require('gridfs-stream');
+
+var database = require(__dirname + '/config/database.yaml');
+database.mongodb_uri = process.env['MONGOHQ_URL'];
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.locals.pretty = true;
 
 app.use(stylus.middleware(__dirname + '/public'));
-
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+app.use(express.cookieParser());
+app.use(express.session({secret: uuid.v4()}));
+
+app.use(express.bodyParser());
+
+app.get('/', function(req, res) {
   res.render('clock');
+});
+
+app.get('/admin', function(req, res) {
+  res.render('admin', {
+    'loggedIn': req.session.loggedIn
+  });
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-	console.log('Listening on port ' + port);
+  console.log('Listening on port ' + port);
+});
+
+MongoClient.connect(database.mongodb_uri, function(err, db) {
+  if (err) throw err;
+});
+
+
+app.post('/admin/login', function(req, res) {
+    if (req.body.password == 'penguins') {
+      req.session.loggedIn = true;
+    }
+    res.redirect('/admin');
 });
