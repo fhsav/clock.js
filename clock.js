ecc9579222caae149a0858c1cd0efdb4291514dd
gluxon <bcheng.gt@gmail.com>
2013-09-02 20:45:20 -0400

    Display data in admin pages

--- clock.js
+++ clock.js
@@ -5,11 +5,16 @@ var format = require('util').format;
 var uuid = require('node-uuid');
 require('js-yaml');
 
-/*var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 var Grid = require('gridfs-stream');
 
+var Themes = require(__dirname + '/lib/themes');
+var Schedules = require(__dirname + '/lib/schedules');
+var Marquee = require(__dirname + '/lib/marquee');
+var Notices = require(__dirname + '/lib/notices');
+
 var database = require(__dirname + '/config/database.yaml');
-database.mongodb_uri = process.env['MONGOHQ_URL'];*/
+database.mongodb_uri = process.env['MONGOHQ_URL'];
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -40,11 +45,35 @@ app.get('/admin/logout', function(req, res) {
 });
 
 app.get('/admin/:page', function(req, res) {
-  res.render('admin', {
+  var data = {
     loggedIn: req.session.loggedIn,
     version: '0.1.0',
     page: req.params.page
-  });
+  };
+
+  if (req.params.page =='themes') {
+    Themes.getAll(function(err, themes) {
+      data.themes = themes;
+      res.render('admin', data);
+    });
+  } else if (req.params.page == 'schedules') {
+    Schedules.getAll(function(err, schedules) {
+      data.schedules = schedules;
+      res.render('admin', data);
+    });
+  } else if (req.params.page == 'marquee') {
+    Marquee.getAll(function(err, marquees) {
+      data.marquees = marquees;
+      res.render('admin', data);
+    });
+  } else if (req.params.page == 'notices') {
+    Notices.getAll(function(err, notices) {
+      data.notices = notices;
+      res.render('admin', data);
+    });
+  } else {
+  res.render('admin', data);
+  }
 });
 
 var port = process.env.PORT || 3000;
@@ -52,9 +81,14 @@ app.listen(port, function() {
   console.log('Listening on port ' + port);
 });
 
-/*MongoClient.connect(database.mongodb_uri, function(err, db) {
+MongoClient.connect(database.mongodb_uri, function(err, db) {
   if (err) throw err;
-});*/
+
+  Themes.setMongoDB(db);
+  Schedules.setMongoDB(db);
+  Marquee.setMongoDB(db);
+  Notices.setMongoDB(db);
+});
 
 app.post('/admin/login', function(req, res) {
   if (req.body.password == 'penguins') {
