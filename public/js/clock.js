59f170f68598383bbcef0789287cac5590d6af32
gluxon <brandoncheng.personal@gmail.com>
2014-09-19 01:58:20 -0400

    Add closing time and responsiveness

--- public/js/clock.js
+++ public/js/clock.js
@@ -2,12 +2,13 @@ $(function() {
   updateTime();
   createMarquee();
   highlightActivePeriod();
+  closingTime();
 });
 
 function updateTime() {
   $("#date").text( moment().format('dddd, MMMM D, YYYY') );
   $("#time").text( moment().format('h:mm:ss') );
-  
+
   setTimeout(updateTime, 1000);
 }
 
@@ -16,7 +17,7 @@ function createMarquee() {
 }
 
 function highlightActivePeriod() {
-  $('#schedule ol li').each(function (index, element) {
+  $('#schedule ol li').each(function(index, element) {
     var startTime = $($(element).children()[0]).attr('datetime');
     var finishTime = $($(element).children()[1]).attr('datetime');
 
@@ -31,6 +32,29 @@ function highlightActivePeriod() {
       $(element).removeClass('active');
     }
   });
-  
+
   setTimeout(highlightActivePeriod, 1000);
+}
+
+function closingTime() {
+  var leaving = true;
+
+  $('#schedule ol li time').each(function(index, element) {
+    var finishTime = $(element).attr('datetime');
+    var finishTimeX = moment(finishTime, "HH:mm:ss").format('X');
+
+    var now = moment().format('X');
+
+    if (now < finishTimeX) {
+      leaving = false;
+    }
+  });
+
+  if (leaving == true) {
+    $('#wrapper').addClass('closingTime');
+  } else {
+    $('#wrapper').removeClass('closingTime');
+  }
+
+  setTimeout(closingTime, 1000);
 }
