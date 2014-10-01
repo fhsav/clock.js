7896f753c09299380cbfc99153f5c2baade5b437
Brandon Cheng <brandoncheng.personal@gmail.com>
2014-10-01 11:35:35 -0400

    Optimize public/js/clock.js and fix schedule collapse visual bugs

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,101 +1,69 @@
 $(function() {
-  updateTime();
-  createMarquee();
-  highlightActivePeriod();
-  closingTime();
-  slideDown();
-  passingTime();
-});
+  // Create Marquee
+  $('#marquee ul').marquee({pauseOnHover: false});
 
-function updateTime() {
-  $("#date").text( moment().format('dddd, MMMM D, YYYY') );
-  $("#time").text( moment().format('h:mm:ss') );
+  function updateClock() {
+    // Update time and date
+    $("#date").text( moment().format('dddd, MMMM D, YYYY') );
+    $("#time").text( moment().format('h:mm:ss') );
 
-  setTimeout(updateTime, 1000);
-}
+    var lastFinishX;
+    var scheduleFinished = true;
 
-function createMarquee() {
-  $("#marquee ul").marquee({ pauseOnHover: false });
-}
+    $('#schedule ol li').each(function(index, element) {
+      var startTime = $($(element).children()[0]).attr('datetime');
+      var finishTime = $($(element).children()[1]).attr('datetime');
 
-function highlightActivePeriod() {
-  $('#schedule ol li').each(function(index, element) {
-    var startTime = $($(element).children()[0]).attr('datetime');
-    var finishTime = $($(element).children()[1]).attr('datetime');
+      var startTimeX = moment(startTime, "HH:mm:ss").format('X');
+      var finishTimeX = moment(finishTime, "HH:mm:ss").format('X');
 
-    var startTimeX = moment(startTime, "HH:mm:ss").format('X');
-    var finishTimeX = moment(finishTime, "HH:mm:ss").format('X');
+      var now = moment().format('X');
 
-    var now = moment().format('X');
+      // Highlight the current period
+      if (startTimeX <= now && now < finishTimeX) {
+        $(element).addClass('active');
+      } else {
+        $(element).removeClass('active');
+      }
 
-    if (startTimeX < now && now < finishTimeX) {
-      $(element).addClass('active');
+      // See if any periods have time left, otherwise it's closing time
+      if (now < finishTimeX) {
+        scheduleFinished = false;
+      }
+
+      // If our schedule is too big, collapse periods
+      if ($('#schedule ol li').size() > 10) {
+        $('#schedule').addClass('collapsing');
+      } else {
+        $('#schedule').removeClass('collapsing');
+      }
+
+      if (finishTimeX <= now && $('#schedule ol li').size() > 10) {
+        $(element).filter(':visible').slideUp('slow');
+      } else {
+        $(element).show();
+      }
+
+      // Show a marker if we're in between periods
+      if (lastFinishX <= now && now < startTimeX) {
+        $(element).addClass('next');
+      } else {
+        $(element).removeClass('next');
+      }
+
+      var lastFinish = $($(element).children()[1]).attr('datetime');
+      lastFinishX = moment(lastFinish, "HH:mm:ss").format('X');
+    });
+
+    // Check if schedule has finished
+    if (scheduleFinished) {
+      $('#wrapper').addClass('closingTime');
     } else {
-      $(element).removeClass('active');
+      $('#wrapper').removeClass('closingTime');
     }
-  });
 
-  setTimeout(highlightActivePeriod, 1000);
-}
-
-function closingTime() {
-  var leaving = true;
-
-  $('#schedule ol li time').each(function(index, element) {
-    var finishTime = $(element).attr('datetime');
-    var finishTimeX = moment(finishTime, "HH:mm:ss").format('X');
-
-    var now = moment().format('X');
-
-    if (now < finishTimeX) {
-      leaving = false;
-    }
-  });
-
-  if (leaving == true) {
-    $('#wrapper').addClass('closingTime');
-  } else {
-    $('#wrapper').removeClass('closingTime');
+    setTimeout(updateClock, 1000);
   }
 
-  setTimeout(closingTime, 1000);
-}
-
-function slideDown() {
-  $('#schedule ol li').each(function(index, element) {
-    var finishTime = $($(element).children()[1]).attr('datetime');
-    var finishTimeX = moment(finishTime, "HH:mm:ss").format('X');
-
-    var now = moment().format('X');
-
-    if (now >= finishTimeX && $('#schedule ol li').size() > 10) {
-      $(element).filter(':visible').slideUp('slow');
-    } else {
-      $(element).show();
-    }
-  });
-
-  setTimeout(slideDown, 1000);
-}
-
-function passingTime() {
-  var lastFinishX;
-  
-  $('#schedule ol li').each(function(index, element) {
-    var startTime = $($(element).children()[0]).attr('datetime');
-    var startTimeX = moment(startTime, "HH:mm:ss").format('X');
-
-    var now = moment().format('X');
-
-    if (lastFinishX < now && now < startTimeX) {
-      $(element).addClass('next');
-    } else {
-      $(element).removeClass('next');
-    }
-
-    var lastFinish = $($(element).children()[1]).attr('datetime');
-    lastFinishX = moment(lastFinish, "HH:mm:ss").format('X');
-  });
-
-  setTimeout(passingTime, 1000);
-}
+  updateClock();
+});
