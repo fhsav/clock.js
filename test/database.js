b4dd6ee931e1dd2a7751de30cf9ea3602b577e65
Brandon Cheng <brandoncheng.personal@gmail.com>
2014-10-11 19:53:43 -0400

    Use bower for js.

--- test/database.js
+++ test/database.js
@@ -8,7 +8,7 @@ var mongodb_uri = datastore.mongodb_uri || process.env.MONGODB_URI || 'localhost
 
 describe('MongoDB', function() {
   it('should connect', function(done) {
-    mongoose.connect(datastore.mongodb_uri);
+    mongoose.connect(mongodb_uri);
 
     var db = mongoose.connection;
     db.on('error', done);
